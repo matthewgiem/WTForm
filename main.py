6402d5db441c2c7a987236a35b426e4b51b690f6
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-11-30 18:35:43 +0700

    add validations to form fields

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, DateField, SubmitField
+from wtforms.validators import DataRequired, InputRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mysecretkey"
 
 
 class LoginForm(FlaskForm):
-    username = StringField(label="UserName")
-    password = PasswordField(label="Password")
-    birthday = DateField(label="Birthday", format='%m/%d/%Y')
+    username = StringField(label="UserName", validators=[DataRequired()])
+    email = StringField(label="Email", validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    birthday = DateField(label="Birthday", format='%Y-%m-%d', validators=[DataRequired()])
     submit = SubmitField(label="Log In")
 
 
@@ -20,11 +22,12 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "GET":
-        login_form = LoginForm()
-        return render_template('login.html', form=login_form)
-    if request.method == "POST":
+    login_form = LoginForm()
+    print("not inside")
+    if login_form.validate_on_submit():
+        print("inside")
         return render_template('index.html')
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':

972dd16636a33f91b6c745e2979f5398eaf3ad18
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-12-04 15:16:11 +0700

    get data from from and validate user

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, DateField, SubmitField
-from wtforms.validators import DataRequired, InputRequired
+from wtforms.validators import DataRequired, ValidationError, Length
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mysecretkey"
 
 
 class LoginForm(FlaskForm):
-    username = StringField(label="UserName", validators=[DataRequired()])
+    username = StringField(label="UserName", validators=[DataRequired(), Length(min=-1, max=8, message="8 max")])
     email = StringField(label="Email", validators=[DataRequired()])
     password = PasswordField(label="Password", validators=[DataRequired()])
     birthday = DateField(label="Birthday", format='%Y-%m-%d', validators=[DataRequired()])
@@ -23,9 +23,13 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    print("not inside")
+    if request.method == "POST":
+        if login_form.email.data == "matthew@gmail.com" and login_form.password.data == "12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     if login_form.validate_on_submit():
-        print("inside")
+        print(login_form.email.data)
         return render_template('index.html')
     return render_template('login.html', form=login_form)
 
